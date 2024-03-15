samollari/LaunchControl

commit 49f1bada500446e955b8503fc47a3d273aa56949

Fix tertiary bug in #1 (undefined colors in canvas creating color definition Detailed in https://github.com/i-am-gizm0/LaunchControl/issues/1#issuecomment-1998899383

src/layout/renderer.ts
@@ -2,11 +2,14 @@ import { LaunchpadModel } from '../launchpad/launchpad';
 import { StandardLEDColorDefinition } from '../launchpad/ledcolor';
 import { callForGrid, range } from '../util';
 import Vector from '../vector';
-import Component, { LocalRenderPixel } from './component';
+import Component, {
+    LocalRenderPixel,
+    LocalRenderPixelMaybeColor,
+} from './component';
 import { LayoutComponent } from './layouts';
 
 export class Canvas {
-    public readonly buffer: number[][];
+    public readonly buffer: (number | undefined)[][];
     public readonly size: Vector;
 
     public constructor(size: Vector) {
@@ -18,7 +21,7 @@ export class Canvas {
         this.buffer[position.x][position.y] = color;
     }
 
-    public get(position: Vector): number {
+    public get(position: Vector): number | undefined {
         return this.buffer[position.x][position.y];
     }
 
@@ -86,6 +89,7 @@ export function renderCanvasToLaunchpad(
                 color: led.color,
             }))
             .filter(notCorners)
+            .filter(definedColors)
             .map(
                 (led) =>
                     new StandardLEDColorDefinition(
@@ -96,13 +100,19 @@ export function renderCanvasToLaunchpad(
     );
 }
 
-function notCorners({ position }: LocalRenderPixel): boolean {
+function notCorners({ position }: LocalRenderPixelMaybeColor): boolean {
     return ![
         new Vector(0, 0),
         new Vector(9, 0),
         new Vector(0, 9),
         new Vector(9, 9),
     ].some((vec) => vec.equals(position));
+}
+
+function definedColors(
+    pixel: LocalRenderPixelMaybeColor,
+): pixel is LocalRenderPixel {
+    return pixel.color !== undefined;
 }
 
 export function createRenderTarget(size: Vector): number[][] {
@@ -113,8 +123,10 @@ export function createRenderTarget(size: Vector): number[][] {
     return pixels;
 }
 
-export function flattenAndLabelPixelMap(canvas: Canvas): LocalRenderPixel[] {
-    const renderedPixels = Array<LocalRenderPixel>();
+export function flattenAndLabelPixelMap(
+    canvas: Canvas,
+): LocalRenderPixelMaybeColor[] {
+    const renderedPixels = Array<LocalRenderPixelMaybeColor>();
     callForGrid(canvas.size, (position) => {
         renderedPixels.push({
             position: position,
@@ -131,7 +143,10 @@ export function copyToPosition(
 ) {
     callForGrid(childCanvas.size, (offset) => {
         const target = position.add(offset);
-        parentCanvas.set(target, childCanvas.get(offset));
+        const childColor = childCanvas.get(offset);
+        if (childColor) {
+            parentCanvas.set(target, childColor);
+        }
     });
 }
 

src/layout/component.ts
@@ -7,6 +7,11 @@ export type LocalRenderPixel = {
     color: number;
 };
 
+export type LocalRenderPixelMaybeColor = {
+    position: Vector;
+    color: number | undefined;
+};
+
 export type RequestRenderFunction = (componentTrace: Component[]) => void;
 
 export default abstract class Component {

commit 3fe875d9eeae78c8db91a794cc0460763b9ae119

Fix type bug introduced in 49f1bad number | undefined was being coerced to boolean (intended effect: if defined. actual effect: if defined and != 0)

src/layout/layouts.ts
@@ -32,9 +32,9 @@ export abstract class LayoutComponent extends Component {
     }
 
     protected partialRenderRequestedChild(componentTrace: Component[]): Canvas {
-        const childComponent = componentTrace[0];
+        const [childComponent, ...restComponentTrace] = componentTrace;
         assertIsElementOf(childComponent, this.components);
-        return Canvas.renderComponent(childComponent, componentTrace.slice(1));
+        return Canvas.renderComponent(childComponent, restComponentTrace);
     }
 }
 
@@ -67,7 +67,7 @@ export class HorizontalLayoutComponent extends LayoutComponent {
         const x = this.components
             .slice(0, requestedChildIndex)
             .map((component) => component.size.x)
-            .reduce(sum);
+            .reduce(sum, 0);
         copyToPosition(renderTarget, childCanvas, new Vector(x, 0));
     }
 
@@ -118,7 +118,7 @@ export class VerticalLayoutComponent extends LayoutComponent {
         const y = this.components
             .slice(0, requestedChildIndex)
             .map((component) => component.size.y)
-            .reduce(sum);
+            .reduce(sum, 0);
         copyToPosition(renderTarget, childCanvas, new Vector(0, y));
     }
 

src/layout/renderer.ts
@@ -144,7 +144,7 @@ export function copyToPosition(
     callForGrid(childCanvas.size, (offset) => {
         const target = position.add(offset);
         const childColor = childCanvas.get(offset);
-        if (childColor) {
+        if (childColor !== undefined) {
             parentCanvas.set(target, childColor);
         }
     });